297d9fb6913227960694f442c3036d88d0088d89
menoo-o <menosuper6@gmail.com>
2024-11-20 14:03:32 +0500

    css dashboard

---
+++ app/ui/dashboard/sidebar/menuLink/MenuLink.tsx
@@ -0,0 +1,11 @@
+import styles from './menuLink.module.css'
+
+export default function MenuLink({item}) {
+  return (
+    <>
+      <div className={styles.container}>
+        
+      </div>
+    </>
+  )
+}
